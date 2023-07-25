9df2b251257e4364c4e7ceb166b4e48e94684469
leo21020458 <124046795+leo21020458@users.noreply.github.com>
2023-07-25 16:52:35 +0800

    介绍

---
+++ src/AlgIntro/Intro1.java
@@ -0,0 +1,9 @@
+package AlgIntro;
+
+/**
+ * 算法是按照一定的条件，从已经存在的数据中进行计算来得到期望的结果
+ *
+ */
+public class Intro1 {
+
+}
--- src/BinarySearchBasic.java
+++ src/BinarySearchBasic.java
@@ -1,3 +1,27 @@
 public class BinarySearchBasic {
+    /**
+     *
+     * @param a - 待查找的升序数组
+     * @param target - 待查找的目标值
+     * @return 找到则返回索引，找不到返回-1
+     * 问题1 为什么是i<=j 而不是 i<j
+     * 因为i和j两个变量可能会相等
+     */
+    public static int binarySearchBasic(int[] a, int target){
+
+        // 设置两个指针和初始值
+        int i = 0;
+        int j = a.length-1;
+        while(i <= j){ // 范围内有东西
+            int midVal = (i+j)/2;
+            if(target<a[midVal]){ // 目标小于中间值，也就是目标在中间值的左边
+                j = midVal - 1;
+            } else if(a[midVal]<target){ // 目标大于中间值，也就是目标在中间值的右边
+                i = midVal + 1;
+            }
+        }
+
+        return -1;
+    }
 
 }
